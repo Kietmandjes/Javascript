ffe67326ed92b830d727f205427ff1011f39edd5
kiet mandjes <32834@ma-web.nl>
2021-11-30 12:41:24 +0100

    les 2 werkt

--- web/web/les2-booschappen/script.js
+++ web/web/les2-booschappen/script.js
@@ -1,1 +1,21 @@
 console.log("script is geladen")
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'paars';
+    fruitvak.appendChild(nieuwElement);
+}
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement);
+}
